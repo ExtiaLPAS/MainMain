705a56c76c1d34aa9686419e912a0e22f0041d63
unknown <raphael.amorim.goncalves@gmail.com>
2020-03-11 16:21:29 +0100

    Implement NightLight Component

--- components/NightLight.js
+++ components/NightLight.js
@@ -1,12 +1,52 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Image, Switch } from 'react-native'
+import Bottom from './Bottom'
 
 class NightLight extends React.Component {
 
+  constructor(props) {
+    super(props)
+    this.state = {
+      click: false,
+      status: 'checked'
+    }
+  }
+
+
+  clicked() {
+    this.setState({
+      click: !this.state.click
+    })
+  }
   render() {
+    let textContent, path;
+
+    if (this.state.click) {
+      textContent = 'Les yeux de main main sont allumées !';
+      path = require('../assets/main_main_on.png');
+
+    } else {
+      textContent = 'Allume les yeux de main main';
+      path = require('../assets/main_main.png');
+    }
+
     return (
-      <View>
-        <Text>NightLight ON/OFF</Text>
+      <View style={{ flex: 1, backgroundColor: 'white' }}>
+        <Text style={{ margin: 30, textAlign: 'center', borderColor: 'black', borderRadius: 5, borderWidth: 1, padding: 10 }}>{textContent}</Text>
+        <View style={{ flex: 2.5, justifyContent: 'center', alignItems: 'center'}}>
+          <Image
+            style={{ width: 200, height: 190 }}
+            source={path}
+          />
+        </View>
+        <View style={{ flex: 0.4 ,alignItems: 'flex-end', borderColor: 'black'}}>
+          <Switch
+          style={{marginRight: 30}}
+            value={this.state.click}
+            onValueChange={() => this.clicked()}
+          />
+        </View>
+        <Bottom />
       </View>
     )
   }
