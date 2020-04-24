338334ab4cbe7ec4ce699492c8f3d6d0b01d87af
Julien LUNA <julien.luna@outlook.com>
2020-04-24 15:37:52 +0200

    Update HomePage.js

--- Components/HomePage.js
+++ Components/HomePage.js
@@ -5,7 +5,7 @@ class HomePage extends React.Component {
     render(){
         return(
             <View  style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                
+                <Text> Salut </Text>
             </View>
         )
     }
